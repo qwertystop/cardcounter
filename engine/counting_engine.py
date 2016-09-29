161dc3fbeb592bc6bc96974cc7b663feb177973e
David Heyman <d.heyman256@gmail.com>
2016-09-28 22:55:17 -0400

    counting engine wrapper to ignore suit

--- engine/counting_engine.py
+++ engine/counting_engine.py
@@ -1,13 +1,18 @@
 """
 Core system for counting cards
 """
-from card import full_deck, Card
+from card import full_deck, Card, Suit
 from typing import Iterable, Optional
 
-def odds_of(*, known: Optional[Iterable[Card]]=None, target: Optional[Card]=None, joker: bool=False) -> float:
+def odds_of(*, known: Optional[Iterable[Card]]=None, target: Optional[Card]=None,
+            joker: bool=False) -> float:
     """
     Calculate odds of a single card not in known (from standard 52- or 54-card deck) being in target
     """
     unknown = [c for c in full_deck(joker) if c not in known]
 
     return len(c for c in unknown if c in target) / len(unknown)
+
+def odds_by_rank(*, known, targetrank, joker):
+    """Wrapper to ignore suit in odds calculation"""
+    return sum(odds_of(known, (targetrank, s), joker) for s in Suit)
